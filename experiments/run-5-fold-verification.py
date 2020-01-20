30a836110543a08ab13c37c040a34c653fc4ac04
Joe Robinson <robinson.jo@husky.neu.edu>
2020-01-20 12:11:44 -0500

    Updated 5-fold experiment

--- experiments/run-5-fold-verification.py
+++ experiments/run-5-fold-verification.py
@@ -41,39 +41,17 @@ for fold in folds:
     dir_fold = f"{dir_out}fold{fold}/"
     mkdir(dir_fold)
 
-    # # all_feats = {**tuple(trfeats1), **tuple(trfeats2)}
-    # print("Normalizing Features")
-    # feat_vecs = np.array(set(feats))
-    # std_scale = skpreprocess.StandardScaler().fit(feat_vecs)
-    # X_train = std_scale.transform(feat_vecs)
-    #
-    # print("PCA")
-    # # pca [default k is 200]
-    # X_scaled, evals, evecs = futils.pca(X_train)
-    #
-    # # normalize test set
-    # X_feats_1 = std_scale.transform(test_feats1)
-    # X_feats_2 = std_scale.transform(test_feats2)
-    #
-    # # apply PCA
-    # X_scaled_1 = np.dot(evecs.T, X_feats_1.T).T
-    # X_scaled_2 = np.dot(evecs.T, X_feats_2.T).T
-
-    # cosine_sim = pw.cosine_similarity(X_scaled_1, X_scaled_2)
-    # scores = np.diag(cosine_sim)
     scores = datatable.loc[ids_test, 'score'].values
     labels = datatable.loc[ids_test, 'label'].values.astype(np.int)
 
     p1 = datatable.loc[ids_test, 'p1'].values
     p2 = datatable.loc[ids_test, 'p2'].values
     t1 = datatable.loc[ids_test, 'type'].values
-    # t2 = datatable.loc[ids_test, 't2'].values
-    # val_labels = [int(pair[1]) for pair in pair_val_list]
 
     ts_matches.append(labels)
     sim.append(scores)
+
     # Compute ROC curve and ROC area
-    # if len(thresholds) == 0:
     fpr, tpr, thresholds = roc_curve(labels, scores)
     ids = np.argsort(scores)
     scores = scores[ids]
@@ -82,17 +60,7 @@ for fold in folds:
     p1 = p1[ids]
 
     t1 = t1[ids]
-    # t2 = t2[ids]
 
-
-
-
-    def reindex(df):
-        df.index = range(len(df))
-
-
-    # else:
-    #     fpr, tpr, _ = roc_curve(labels, scores, thresholds)
     roc_auc = auc(fpr, tpr)
     print(f"Acc:{roc_auc}")
     print('Saving Results')
@@ -102,8 +70,6 @@ for fold in folds:
     np.savetxt(f"{dir_fold}tpr.csv", tpr)
     np.savetxt(f"{dir_fold}roc_auc.csv", [roc_auc])
     aucs.append(roc_auc)
-    # fpr, tpr, _ = roc_curve(np.array(ts_matches).flatten(), np.array(sim).flatten())
-    # roc_auc = auc(fpr, tpr)
 
 mean_tpr = np.mean(tprs, axis=0)
 mean_tpr[-1] = 1.0
