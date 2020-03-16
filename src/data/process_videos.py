b0815aa9ca76c70e3d10a47bb39180061e6ceff4
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-03-16 17:58:47 -0400

    added iou calculation and loop to compare neighboring frames
    Signed-off-by: Joseph Robinson <robinson.jo@husky.neu.edu>

--- src/data/process_videos.py
+++ src/data/process_videos.py
@@ -1,23 +1,22 @@
 import glob
 import shutil
+from pathlib import Path
 
 from src.data.videos import get_video_metadata
 
 
-def bb_intersection_over_union(boxA, boxB):
+def bb_intersection_over_union(box1, box2):
     # determine the (x, y)-coordinates of the intersection rectangle
-    xA = max(boxA[0], boxB[0])
-    yA = max(boxA[1], boxB[1])
-    xB = min(boxA[2], boxB[2])
-    yB = min(boxA[3], boxB[3])
+    xA = max(box1[0], box2[0])
+    yA = max(box1[1], box2[1])
+    xB = min(box1[2], box2[2])
+    yB = min(box1[3], box2[3])
     # compute the area of intersection rectangle
     interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
     # compute the area of both the prediction and ground-truth rectangles
-    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
-    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
-    # compute the intersection over union by taking the intersection
-    # area and dividing it by the sum of prediction + ground-truth
-    # areas - the interesection area
+    boxAArea = (box1[2] - box1[0] + 1) * (box1[3] - box1[1] + 1)
+    boxBArea = (box2[2] - box2[0] + 1) * (box2[3] - box2[1] + 1)
+    # compute IoU by taking the intersection area and dividing it by the sum of BBs A + B areas - the intersection area
     iou = interArea / float(boxAArea + boxBArea - interArea)
     # return the intersection over union value
     return iou
@@ -68,12 +67,42 @@ cfaces = [f for f in f_metas if f.count(i)]
 
 import pandas as pd
 
-for k, cface in enumerate(cfaces):
-    df_meta = pd.read_json(cfaces[0])
-    df_meta.columns = df_meta.iloc[0]
-    df_meta = df_meta.drop(0)
-    df_meta.reset_index(inplace=True)
-    l, t, r, b = tuple(df_meta.iloc[0].bb)
+nframes = meta['frame_count']
+
+import numpy as np
+
+cur_track = 0
+
+ids = [int(i) for i in ids]
+# for k, cface in enumerate(cfaces):
+li_track_ids = []
+
+for i in ids:
+    track_ids = -1 * np.ones((nframes,))
+    box_prev = 0
+    for k in range(nframes):
+        fin = '{0}meta/fr{1:06d}_face{2:02d}.json'.format(dir_video, k, i)
+
+        if Path(fin).is_file():
+            df_meta = pd.read_json(fin)
+            df_meta.columns = df_meta.iloc[0]
+            df_meta = df_meta.drop(0)
+            df_meta.reset_index(inplace=True)
+            l, t, r, b = tuple(df_meta.iloc[0].bb)
+            box_cur = l, t, r, b
+            if box_prev:
+                iou = bb_intersection_over_union(box_cur, box_prev)
+
+                track_ids[k] = iou
+
+                # if iou < .75:
+                #     cur_track += 1
+                # track_ids[k][1] = cur_track
+
+            box_prev = box_cur
+        else:
+            track_ids[k] = 0
+    li_track_ids.append(track_ids)
 
 df_meta.iloc[0] = df_meta.iloc[1]
 
