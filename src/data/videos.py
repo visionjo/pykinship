fecb3aaa27a60d3a97d25c4d4f6cb5df094558be
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-03-16 02:11:11 -0400

    process set of subjects per videos that exist.
    Signed-off-by: Joseph Robinson <robinson.jo@husky.neu.edu>

--- src/data/videos.py
+++ src/data/videos.py
@@ -14,19 +14,22 @@ fetch_videos = False
 
 
 def wget_video(
-        name, url, cmd="youtube-dl --continue --merge-output-format mp4 --all-subs -o {} {}"
+        name, url, cmd="youtube-dl --continue --merge-output-format mp4 --all-subs -o {} {}", dir_out=None
 ):
     """
     Fetch video from youtube
+    :param dir_out: directory to save to - if directory does not exist, then sets to save in current directory,
     :param name: identifier to name video with
     :param url: youtube URL to download as MP4
     :param cmd: command line call (Note, str.format() assumes 2 placeholders
     :return: True if successfully downloaded; else, return False.
     """
+    if not Path(dir_out).is_dir():
+        dir_out = ''
 
     try:
 
-        os.system(cmd.format(name, url))
+        os.system(cmd.format(dir_out + name, url))
         return True
     finally:
         return False
@@ -35,118 +38,172 @@ def wget_video(
 def encode_face_files(imfiles):
     images = OrderedDict({impath: cv2.imread(impath)[:, :, ::-1] for impath in imfiles})
 
-    return {
-        path: face_recognition.face_encodings(images[path])[0] for path in images.keys()
-    }
-
-
-def fetch_videos(f_csv):
-    df = pd.read_csv(f_csv)
-    df["ref"] = df["firstname"] + "_" + df["surname"]
-    df.apply(lambda x: wget_video(x["ref"], x["video"]), axis=1)
-
-
-def encode_mids(f_video=None, f_encodings=None, save_pickle=True):
-    if Path(f_encodings).is_file():
-        encodings = pd.read_pickle(f_encodings)
-    else:
-        impaths = glob.glob(f"{str(Path(f_video).parent)}/*.jpg")
-        encodings = encode_face_files(impaths)
-        if save_pickle:
-            pd.to_pickle(encodings, encodings)
+    encodings = {}
+    for impath, image in images.items():
+        try:
+            encodings[impath] = face_recognition.face_encodings(image)[0]
+        except:
+            print(f'Error encoding {impath}')
     return encodings
 
 
+def read_family_member_list(f_csv):
+    df = pd.read_csv(f_csv)
+    # df['last'] = df["surname"].apply(lambda x: x.split('.')[0])
+    df["ref"] = df["firstname"] + "_" + df["surname"]
+    df = df.loc[df.video.notna()]
+    df.reset_index(inplace=True)
+    del df['index']
+    return df
+
+
+def fetch_videos(df, dir_out=None):
+    df.apply(lambda x: wget_video(x["ref"], x["video"], dir_out=dir_out), axis=1)
+
+
+def encode_mids(d_mid, f_encodings=None, save_pickle=True):
+    if f_encodings and Path(f_encodings).is_file():
+        encodings = pd.read_pickle(f_encodings)
+    else:
+        impaths = glob.glob(f"{d_mid}/*.jpg")
+        encodings = encode_face_files(impaths)
+        if save_pickle:
+            f_encodings = f"{d_mid}/encodings.pkl"
+            pd.to_pickle(encodings, f_encodings)
+    return encodings
+
+
+def crop_detection(face, locations):
+    w, h = locations['bb'][2] - locations['bb'][0], locations['bb'][3] - locations['bb'][1]
+    left, right, bottom, top = (
+        locations['bb'][0] - w * 0.1,
+        locations['bb'][2] + w * 0.1,
+        locations['bb'][3] + h * 0.1,
+        locations['bb'][1] - h * 0.1,
+    )
+    return face.crop([left, top, right, bottom])
+
+
+def get_video_metadata(f_video):
+    cap = cv2.VideoCapture(f_video)
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count / fps
+    print(f'{f_video} meta:\n=====')
+    print(f'fps = {fps}')
+    print(f'number of frames = {frame_count}')
+    print(f'duration (S) = {duration}')
+    print(f'duration (M:S) = {int(duration / 60)}:{duration % 60}')
+    cap.release()
+
+
 def process_subject(encodings, f_video, dir_out):
+    get_video_metadata(f_video)
     video_capture = cv2.VideoCapture(f_video)
-    # impaths = Path(dir_mid).glob('*.jpg')
+    # Check if camera opened successfully 
+    if not video_capture.isOpened():
+        print("Error opening video  file")
+        return
 
-    while True:
-
-        # Grab a single frame of video
-        ret, frame = video_capture.read()
-        # if Path(f"{dout}fr{frame_id}_face{0}.png").exists():
-        #     # face_landmarks_list = face_recognition.face_landmarks(frame[:, :, ::-1])
-        #     # pd.DataFrame().from_dict(face_landmarks_list[0].items()).T.to_json(f"{dout}fr{frame_id}_face{0}-landmarks.json")
-        #     frame_id += 1
-        #     continue
+    frame_id = 0
+    # Read until video is completed 
+    while video_capture.isOpened():
         print(Path(f"fr{frame_id}_face{0}.png"))
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_frame = frame[:, :, ::-1]
 
-        # Find all the faces in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_frame, model="cnn")
+        print(dir_out)
+        # Capture frame-by-frame 
+        ret, frame = video_capture.read()
+        if ret:
+            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+            rgb_frame = frame[:, :, ::-1]
+            # Find all the faces in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_frame, model="cnn")
 
-        # Initialize variables
-        # face_locations_all = []
-        frame_id = 0
-        # Display the results
-        for j, (top, right, bottom, left) in enumerate(face_locations):
-            # Draw a box around the face
-            face_locations_all = {
-                "frame": frame_id,
-                "face": j,
-                "bb": (left, top, right, bottom),
-                "landmarks": face_locations[j],
-            }
+            # Initialize variables
+            # face_locations_all = []
+            # frame_id = 0
+            # Display the results
+            for j, (top, right, bottom, left) in enumerate(face_locations):
+                # Draw a box around the face
+                face_locations_dict = {
+                    "frame": frame_id,
+                    "face": j,
+                    "bb": (left, top, right, bottom),
+                    "landmarks": face_locations[j],
+                }
 
-            face = Image.fromarray(rgb_frame)
-            w, h = right - left, bottom - top
-            left, right, bottom, top = (
-                left - w * 0.1,
-                right + w * 0.1,
-                bottom + h * 0.1,
-                top - h * 0.1,
-            )
-            face = face.crop([left, top, right, bottom])
-
-            try:
-                unknown_encoding = face_recognition.face_encodings(np.array(face))
+                face_image = crop_detection(Image.fromarray(rgb_frame), face_locations_dict)
+                # try:
+                unknown_encoding = face_recognition.face_encodings(np.array(face_image))
                 #     cv2.cvtColor(np.array(face), cv2.COLOR_RGB2BGR)
                 # )
                 if not len(unknown_encoding):
                     continue
                 unknown_encoding = unknown_encoding[0]
                 results = face_recognition.compare_faces(encodings, unknown_encoding)
-                face.save(f"{dir_out}fr{frame_id}_face{j}.png")
+                face_image.save(f"{dir_out}faces/fr{frame_id}_face{j}.png")
                 pd.to_pickle(
-                    unknown_encoding, f"{dir_out}fr{frame_id}_face{j}-encoding.csv"
+                    unknown_encoding, f"{dir_out}encodings/fr{frame_id}_face{j}-encoding.csv"
                 )
                 pd.DataFrame(results).astype(int).to_csv(
-                    f"{dir_out}fr{frame_id}_face{j}-predictions.csv",
+                    f"{dir_out}predictions/fr{frame_id}_face{j}-predictions.csv",
                     header=None,
                     index=False,
                 )
-                pd.DataFrame().from_dict(face_locations_all.items()).T.to_json(
-                    f"{dir_out}fr{frame_id}_face{j}-meta.json"
+                pd.DataFrame().from_dict(face_locations_dict.items()).T.to_json(
+                    f"{dir_out}meta/fr{frame_id}_face{j}-meta.json"
                 )
 
                 print(results)
-            except:
+                # except:
                 print(f"{dir_out}fr{frame_id}_face{j}.png")
-            finally:
+                # finally:
 
                 # cv2.imwrite(f"{dout}fr{frame_id}_face{j}.png", face)
                 # cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-                frame_id += 1
+            frame_id += 1
+        else:
+            break
+
+    # When everything done, release the video capture object
+    video_capture.release()
+
+
+def process_sample(df, dir_videos, dir_encodings):
+    f_video = dir_videos + df['ref'] + '.mp4'
+    if not Path(f_video).is_file():
+        return
+    print(f_video)
+    dir_mid = f"{dir_encodings}{df['fid']}/MID{df['mid']}/"
+    encodings = encode_mids(dir_mid)
+    encodings = list(encodings.values())
+    dout = f"{dir_videos}{df['ref']}/"
+    Path(dout).mkdir(exist_ok=True)
+    Path(dout).joinpath('encodings').mkdir(exist_ok=True)
+    Path(dout).joinpath('faces').mkdir(exist_ok=True)
+    Path(dout).joinpath('predictions').mkdir(exist_ok=True)
+    Path(dout).joinpath('meta').mkdir(exist_ok=True)
+    process_subject(encodings, f_video, dout)
 
 
 def main():
     f_urls = "../../data/family_members.csv"
-
-    if fetch_videos:
-        fetch_videos(f_urls)
-
     f_video = "../../data/videos-fiw/Ben_Affleck.mp4"
     f_encodings = f"{str(Path(f_video).parent)}/mid-encodings.pkl"
-    d_mid = Path(f_video).parent
+    dir_out = "/home/jrobby/kinship/"
+    dir_fids = '/home/jrobby/master-version/fiwdb/FIDs/'
+    df = read_family_member_list(f_urls)
+    fetch_videos = False
+    if fetch_videos:
+        fetch_videos(df, dir_out)
 
-    encodings = encode_mids(f_video=f_video, f_encodings=f_encodings)
-    encodings = list(encodings.values())
-    dout = f"{str(Path(f_video).parent)}/frames/"
-    Path(dout).mkdir(exist_ok=True)
-
-    process_subject(encodings, f_video, dout)
+    # f_videos = Path(dir_videos).glob('*.mp4')
+    # d_mid = Path(f_video).parent
+    nsubjects = len(df)
+    for i in range(nsubjects):
+        process_sample(df.iloc[i], dir_out, dir_fids)
+    # df.apply(lambda x: , axis=1)
+    # process_se(df)
 
 
 # import pickle
