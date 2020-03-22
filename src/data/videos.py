bbd39f65f583ea9973956e3474fc9438197ac1ca
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-03-22 11:03:33 -0400

    processing scenes in videos.py
    Signed-off-by: Joseph Robinson <robinson.jo@husky.neu.edu>

--- src/data/videos.py
+++ src/data/videos.py
@@ -1,20 +1,19 @@
 # import libraries
 import glob
-import os
 from collections import OrderedDict
-from pathlib import Path
 
 import cv2
 import face_recognition
 import numpy as np
 import pandas as pd
 from PIL import Image
-
-fetch_videos = False
+from pathlib import Path
 
 
 def wget_video(
-        name, url, cmd="youtube-dl --continue --merge-output-format mp4 --all-subs -o {} {}", dir_out=None
+        name, url,
+        cmd="youtube-dl --continue --write-auto-sub --get-thumbnail --write-all-thumbnails --get-description --all-subs -o {} {}",
+        dir_out=None
 ):
     """
     Fetch video from youtube
@@ -28,10 +27,11 @@ def wget_video(
         dir_out = ''
 
     try:
-
-        os.system(cmd.format(dir_out + name, url))
+        if not glob.glob(dir_out + name + '/*.mp4') + glob.glob(dir_out + name + '/*.mkv'):
+            os.system(cmd.format(dir_out + name, url))
         return True
     finally:
+        print(name)
         return False
 
 
@@ -61,7 +61,7 @@ def fetch_videos(df, dir_out=None):
     df.apply(lambda x: wget_video(x["ref"], x["video"], dir_out=dir_out), axis=1)
 
 
-def encode_mids(d_mid, f_encodings=None, save_pickle=True):
+def encode_mids(d_mid, f_encodings=None, save_pickle=False):
     if f_encodings and Path(f_encodings).is_file():
         encodings = pd.read_pickle(f_encodings)
     else:
@@ -106,7 +106,7 @@ def print_video_metadata(f_video):
 
 
 def process_subject(encodings, f_video, dir_out):
-    get_video_metadata(f_video)
+    print_video_metadata(f_video)
     video_capture = cv2.VideoCapture(f_video)
     # Check if camera opened successfully
     if not video_capture.isOpened():
@@ -182,32 +182,94 @@ def process_subject(encodings, f_video, dir_out):
     video_capture.release()
 
 
-def process_sample(df, dir_videos, dir_encodings):
+import os
+
+
+def process_sample(df, dir_videos, encodings=None):
     f_video = dir_videos + df['ref'] + '.mp4'
+    dout = f"{dir_videos}{df['ref']}/"
+    if Path(dout).is_dir():
+        return
+    
     if not Path(f_video).is_file():
         return
     print(f_video)
-    dir_mid = f"{dir_encodings}{df['fid']}/MID{df['mid']}/"
-    encodings = encode_mids(dir_mid)
-    encodings = list(encodings.values())
-    dout = f"{dir_videos}{df['ref']}/"
+
     Path(dout).mkdir(exist_ok=True)
     Path(dout).joinpath('encodings').mkdir(exist_ok=True)
     Path(dout).joinpath('faces').mkdir(exist_ok=True)
     Path(dout).joinpath('predictions').mkdir(exist_ok=True)
     Path(dout).joinpath('meta').mkdir(exist_ok=True)
     process_subject(encodings, f_video, dout)
+    os.remove(f_video)
 
 
-def main():
+def process_scenes(dir_in, dir_out, encodings=None):
+    """ process first, middle, and last image of each scene to determine whether MID is present.
+    :param
+    """
+    Path(dir_out).mkdir()
+    imfiles = glob.glob(dir_in + '*.jpg')
+    imfiles.sort()
+    imfiles = np.reshape(imfiles, (-1, 3))
+    for shot_id, imstack in enumerate(imfiles):
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        for face_id, imfile in enumerate(imstack):
+            suffix = imfile.split('.')[-2][-7:]
+            frame = cv2.imread(imfile)
+            rgb_frame = frame[:, :, ::-1]
+            # Find all the faces in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_frame, model="cnn")
+
+            for j, (top, right, bottom, left) in enumerate(face_locations):
+                # Draw a box around the face
+                face_locations_dict = {
+                    "frame": shot_id,
+                    "face": j,
+                    "bb": (left, top, right, bottom),
+                    "landmarks": face_locations[j],
+                }
+
+                face_image = crop_detection(Image.fromarray(rgb_frame), face_locations_dict)
+                # try:
+                unknown_encoding = face_recognition.face_encodings(np.array(face_image))
+                #     cv2.cvtColor(np.array(face), cv2.COLOR_RGB2BGR)
+                # )
+                if not len(unknown_encoding):
+                    continue
+                unknown_encoding = unknown_encoding[0]
+                results = face_recognition.compare_faces(encodings, unknown_encoding)
+                face_image.save(f"{dir_out}s{suffix}-{j}.png")
+                pd.to_pickle(
+                    unknown_encoding, f"{dir_out}s{suffix}-{j}-encoding.csv"
+                )
+                pd.DataFrame(results).astype(int).to_csv(
+                    f"{dir_out}s{suffix}-{j}-predictions.csv",
+                    header=None,
+                    index=False,
+                )
+                pd.DataFrame().from_dict(face_locations_dict.items()).T.to_json(
+                    f"{dir_out}s{suffix}-{j}-meta.json"
+                )
+
+                print(results)
+                # except:
+                print(f"{dir_out}s{suffix}-{j}.png")
+                # finally:
+
+
+# def main():
+if __name__ == "__main__":
     f_urls = "../../data/family_members.csv"
     f_video = "../../data/videos-fiw/Ben_Affleck.mp4"
     f_encodings = f"{str(Path(f_video).parent)}/mid-encodings.pkl"
-    dir_out = "/home/jrobby/kinship/"
-    dir_fids = '/home/jrobby/master-version/fiwdb/FIDs/'
+    dir_out = "/Volumes/MySpace/kinship/"
+    dir_scenes = '/home/jrobby/kinship/'
+    dir_fids = '/home/jrobby/master-version/fiwdb/FIDs/'  # '/Users/jrobby/data/FIDs/'
     df = read_family_member_list(f_urls)
     get_videos = False
-    process_videos = True
+    process_videos = False
+    procese_shots = True
     if get_videos:
         fetch_videos(df, dir_out)
 
@@ -216,19 +278,33 @@ def main():
     if process_videos:
         nsubjects = len(df)
         for i in range(nsubjects):
-            process_sample(df.iloc[i], dir_out, dir_fids)
+            dir_mid = f"{dir_fids}{df['fid']}/MID{df['mid']}/"
+            encodings = encode_mids(dir_mid)
+            encodings = list(encodings.values())
+            process_sample(df.iloc[i], dir_out, encodings)
     # df.apply(lambda x: , axis=1)
     # process_se(df)
+    if procese_shots:
+        dir_data = '/home/jrobby/kinship/'
+        dirs_scenes = glob.glob(dir_data + '*/scenes/')
+        dirs_scenes.sort()
 
+        subjects = [d.replace(dir_data, '').replace('/scenes/', '') for d in dirs_scenes]
 
-# import pickle
-# with open(dout + "flandmarks.pkl", 'wb') as f:
-#     pickle.dump(f)
-# pd.to_pickle(face_locations_all, )
+        for subject in subjects:
+            dout = dir_scenes + subject + '/scenes/faces/'
+            print(subject)
+            if not (subject in df.ref.to_list() and not Path(dout).exists()):
+                continue
+            mid = df.loc[df.ref == subject, 'mid'].values[0]
+            fid = df.loc[df.ref == subject, 'fid'].values[0]
+            dir_mid = f"{dir_fids}{fid}/MID{mid}/"
+            encodings = encode_mids(dir_mid)
+            encodings = list(encodings.values())
+            process_scenes(dir_scenes + subject + '/scenes/', dout, encodings)
 
+    # if __name__ == "__main__":
+    # main()
 
-# virtualenv --no-site-packages ~/.virtualenvs/RingNet
-# source ~/.virtualenvs/RingNet/bin/activate
-# pip install --upgrade pip==19.1.1
-if __name__ == "__main__":
-    main()
+    # cmd = 'youtube-dl  --write-description --continue --write-sub --write-auto-sub --all-subs --write-all-thumbnails ' \
+    # '-o {}  {}'
