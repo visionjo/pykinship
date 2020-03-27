ecdb050814771aa7f6ccc7d8d8d36aa07df82ea2
Joe Robinson <robinson.jo@husky.neu.edu>
2020-03-27 18:40:53 -0400

    updated extract_wav_from_mp4.py

--- scripts/extract_wav_from_mp4.py
+++ scripts/extract_wav_from_mp4.py
@@ -5,13 +5,18 @@ from pathlib import Path
 import moviepy.editor as mp
 from tqdm import tqdm
 
-dir_data = '/Volumes/MyWorld/FIW_Video/data/processed/'
+dir_data = '../data/fiw-videos/processed/'
 f_scenes = glob.glob(f"{dir_data}*/*/scenes/*.mp4")
 
 dirs_out = [Path(f).parent.joinpath('audio') for f in f_scenes]
 _ = [d.mkdir(exist_ok=True) for d in dirs_out]
 
-for f_scene in tqdm(f_scenes):
-    clip = mp.VideoFileClip(f_scene)
+for f_scene in tqdm(reversed(f_scenes[:-int(len(f_scenes) / 2)])):
     fout = f_scene.replace('scenes/', 'scenes/audio/').replace('.mp4', '.wav')
-    clip.audio.write_audiofile(fout)
+    if not Path(fout).is_file():
+        try:
+            clip = mp.VideoFileClip(f_scene)
+
+            clip.audio.write_audiofile(fout)
+        except OSError:
+            print(f_scene)
