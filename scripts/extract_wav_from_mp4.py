dd16bbdd5353850737eb3ec11d2908522f1cf455
Joe Robinson <robinson.jo@husky.neu.edu>
2020-03-27 18:46:04 -0400

    updated extract_wav_from_mp4.py

--- scripts/extract_wav_from_mp4.py
+++ scripts/extract_wav_from_mp4.py
@@ -11,7 +11,7 @@ f_scenes = glob.glob(f"{dir_data}*/*/scenes/*.mp4")
 dirs_out = [Path(f).parent.joinpath('audio') for f in f_scenes]
 _ = [d.mkdir(exist_ok=True) for d in dirs_out]
 
-for f_scene in tqdm(reversed(f_scenes[:-int(len(f_scenes) / 2)])):
+for f_scene in tqdm(f_scenes):
     fout = f_scene.replace('scenes/', 'scenes/audio/').replace('.mp4', '.wav')
     if not Path(fout).is_file():
         try:
