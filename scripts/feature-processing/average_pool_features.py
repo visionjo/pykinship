7bbeff03d0e18982c15605ffdabb23241bd11697
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-04-30 01:24:56 -0400

    updated average_pool_features.py
    Signed-off-by: Joseph Robinson <robinson.jo@husky.neu.edu>

--- scripts/feature-processing/average_pool_features.py
+++ scripts/feature-processing/average_pool_features.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import numpy as np
 from tqdm import tqdm
 
@@ -6,17 +7,19 @@ path_tracks = Path("/Volumes/MyWorld/FIW-MM/clips-tp-faces")
 for fid in tqdm(list(path_tracks.iterdir())):
     if ".zip" in str(fid):
         continue
-    for mid in Path(fid).iterdir():
-        for vid in Path(mid).iterdir():
-            if Path(vid).is_dir():
-                for vdir in Path(vid).iterdir():
-                    if Path(vdir).is_dir():
+    for mid in fid.iterdir():
+        if not mid.is_dir():
+            continue
+        for vid in mid.iterdir():
+            if vid.is_dir():
+                for vdir in vid.iterdir():
+                    if vdir.is_dir():
                         fout = vdir / "avg_encoding.npy"
-                        # if fout.is_file():
-                        #     print('skip')
-                        #     continue
+                        if fout.is_file():
+                            print("skip")
+                            continue
                         paths_encodings = []
-                        for f_feature in Path(vdir).glob("v*/*.npy"):
+                        for f_feature in vdir.glob("v*/*.npy"):
                             if "encoding" in f_feature.name:
                                 continue
                             print(f_feature)
