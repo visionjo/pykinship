39cc953f110ab67a2f179915c992279b7fb75b03
Joseph Robinson <robinson.jo@husky.neu.edu>
2020-04-30 01:49:39 -0400

    Update template_generator.py

---
+++ src/tools/template_generator.py
@@ -0,0 +1,16 @@
+"""
+ TODO - integrate into script.
+"""
+
+import numpy as np
+
+
+def compute_mean(features):
+    if not isinstance(features, np.ndarray):
+        features = np.concatenate(features)
+
+    embed_size = features.shape[-1]
+    if len(features.shape) != 2:
+        features = features.reshape((-1, embed_size))
+
+    return features.mean(axis=0).reshape((1, -1))
